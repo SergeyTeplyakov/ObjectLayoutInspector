01364acadb123edf00ebebdda7a61f0f6a1f01fa
Sergey Tepliakov <seteplia@microsoft.com>
2017-09-19 17:48:19 -0700

    Switch to a different approach for getting a struct size

--- src/ObjectLayoutInspector/ObjectLayoutInspector/InspectorHelper.cs
+++ src/ObjectLayoutInspector/ObjectLayoutInspector/InspectorHelper.cs
@@ -4,9 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
-using System.Security;
 using System.Text;
 
 namespace ObjectLayoutInspector
@@ -80,18 +78,38 @@ namespace ObjectLayoutInspector
         {
             Debug.Assert(type.IsValueType);
             // Generate a struct with two fields of type 'type'
-            var generatedType = GenerateStructWithTwoFields(type);
+            var generatedType = GenerateSizeComputerOf(type);
             // The offset of the second field is the size of 'type'
             var fieldsOffsets = GetFieldOffsets(generatedType);
             return fieldsOffsets[1].offset;
         }
 
-        private static Type GenerateStructWithTwoFields(Type fieldType)
+        /// <summary>
+        /// Helper struct that is used for computing the size of a struct.
+        /// </summary>
+        struct SizeComputer<T>
         {
-            var returnType = typeof(System.ValueTuple<,>).MakeGenericType(fieldType, fieldType);
+            public T field1;
+            public T field2;
+        }
+
+        /// <summary>
+        /// Method "generates" a <code>SizeComputer&lt;FieldType&gt;</code> type at runtime.
+        /// </summary>
+        /// <remarks>
+        /// Instead of generating a real type using <see cref="ModuleBuilder"/> for defining a new type this function
+        /// generates a method that returns an empty instance of <code>SizeComputer&lt;FieldType&gt;</code>.
+        /// There is a security issue with <see cref="ModuleBuilder"/>. Generated type resides in a new assembly that doesn't have
+        /// a visibility to internal or private types and there is no way to work around this issue.
+        /// So instead of that we're relying on the CLR and a magic of generics to generate a closed generic type at runtime.
+        /// </remarks>
+        private static Type GenerateSizeComputerOf(Type fieldType)
+        {
+            var returnType = typeof(SizeComputer<>).MakeGenericType(fieldType);
 
             var method = new DynamicMethod(
-                name: "GetFieldOffsets",
+                name: "GenerateTypeForSizeComputation",
+                //returnType: returnType,
                 returnType: typeof(object),
                 parameterTypes: new Type[0],
                 m: typeof(InspectorHelper).Module,
@@ -99,33 +117,33 @@ namespace ObjectLayoutInspector
 
             ILGenerator ilGen = method.GetILGenerator();
 
-            // Declaring local variable of type long[]
-            ilGen.DeclareLocal(returnType);
+            // This method generates the following function:
+            // SizeComputer<FieldType> GenerateTypeForSizeComputation()
+            // {
+            //    SizeComputer<FieldType> l1 = default(SizeComputer<FieldType>);
+            //    object l2 = l1; // box the local
+            //    return l2;
+            // }
 
+            ilGen.DeclareLocal(returnType);
+            ilGen.DeclareLocal(typeof(object));
+
+            // SizeComputer<FieldType> l1 = default(SizeComputer<FieldType>);
+            ilGen.Emit(OpCodes.Ldloca_S, 0);
+            ilGen.Emit(OpCodes.Initobj, returnType);
+
+            // object l2 = l1; // box the local
             ilGen.Emit(OpCodes.Ldloc_0);
+            ilGen.Emit(OpCodes.Box, returnType);
+            ilGen.Emit(OpCodes.Stloc_1);
+
+            // return l2;
+            ilGen.Emit(OpCodes.Ldloc_1);
             ilGen.Emit(OpCodes.Ret);
 
-            Func<object> func = (Func<object>) method.CreateDelegate(typeof(Func<object>));
+            var func = (Func<object>) method.CreateDelegate(typeof(Func<object>));
             var result = func();
             return result.GetType();
-            //// Create assembly and module
-            //var assemblyName = new AssemblyName("DynamicAssembly");
-            //var assemblyBuilder =
-            //    AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect, new CustomAttributeBuilder[0],
-            //    SecurityContextSource.CurrentAssembly);
-            //var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
-
-            //// Open a new Type for definition
-            //var typeBuilder = moduleBuilder.DefineType(
-            //    name: "CustomStruct",
-            //    attr: TypeAttributes.SequentialLayout | TypeAttributes.AnsiClass,
-            //    parent: typeof(ValueType));
-
-            //// Define two fields of the same types
-            //typeBuilder.DefineField("field1", fieldType, FieldAttributes.Public);
-            //typeBuilder.DefineField("field2", fieldType, FieldAttributes.Public);
-
-            //return typeBuilder.CreateType();
         }
 
         public static (FieldInfo fieldInfo, int offset)[] GetFieldOffsets<T>()
