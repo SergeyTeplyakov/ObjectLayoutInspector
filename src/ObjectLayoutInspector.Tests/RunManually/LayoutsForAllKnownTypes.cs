14831d4db3c7c5de25133f09e458e79345cf129a
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2022-09-21 17:26:37 -0700

    Add snk file + upgrade projects

--- src/ObjectLayoutInspector.Tests/RunManually/LayoutsForAllKnownTypes.cs
+++ src/ObjectLayoutInspector.Tests/RunManually/LayoutsForAllKnownTypes.cs
@@ -1,14 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
 namespace ObjectLayoutInspector.Tests.RunManually
 {
+    public struct AbsolutePath
+    {
+#pragma warning disable CS0169
+        private int _absolutePath;
+#pragma warning restore CS0169
+    }
+
+    [Flags]
+    internal enum ObservedPathEntryFlags : byte
+    {
+        None = 0,
+        IsSearchPath = 1,
+        IsDirectoryPath = 2,
+        DirectoryEnumeration = 4,
+        DirectoryEnumerationWithCustomPattern = 8,
+        DirectoryEnumerationWithAllPattern = 16,
+        FileProbe = 32
+    }
+
+    public struct ObservedPathEntry
+    {
+        public readonly int AbsolutePath;
+        public readonly string EnumeratePatternRegex;
+#pragma warning disable CS0649
+        internal readonly ObservedPathEntryFlags Flags;
+#pragma warning restore CS0649
+    }
+    
+    [StructLayout(LayoutKind.Explicit)]
+    public struct ObservedPathEntry2
+    {
+        [FieldOffset(0)]
+        public readonly string EnumeratePatternRegex;
+
+        [FieldOffset(8)]
+        public readonly AbsolutePath AbsolutePath;
+
+        [FieldOffset(12)]
+        internal readonly ObservedPathEntryFlags Flags;
+        
+        
+    }
+
     [TestFixture]
     public class LayoutsForAllKnownTypes
     {
+        [Test]
+        public void GetLayoutForTwoSpecificTypes()
+        {
+            TypeLayout.PrintLayout(typeof(ObservedPathEntry));
+            TypeLayout.PrintLayout(typeof(ObservedPathEntry2));
+        }
         struct SizeComputer<T>
         {
             public T field1;
@@ -28,13 +78,6 @@ namespace ObjectLayoutInspector.Tests.RunManually
         {
             public double d; public double d2; public byte b; public byte b2; public byte b3;
             public Test2(byte b, double d, byte b2, double d2, byte b3) => (this.d, this.d2, this.b, this.b2, this.b3) = (d, d2, b, b2, b3);
-        }
-
-        [Test]
-        public void GetLayoutForTwoSpecificTypes()
-        {
-            TypeLayout.PrintLayout(typeof(Test1));
-            TypeLayout.PrintLayout(typeof(Test2));
         }
 
         //[Test]
